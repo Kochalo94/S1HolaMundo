a20eb3afe06628efd5f4459d8226b9fca53f03e1
Harold Arando <harold.arando@gmail.com>
2015-07-10 23:02:41 -0400

    Carga Inicial

---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by Harold Arando on 10/7/15.
+//  Copyright (c) 2015 KochaloSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var miLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func miSaludo(sender: AnyObject) {
+        
+        
+        miLabel.text = "Hola Mundo"
+        
+    }
+
+}
+
